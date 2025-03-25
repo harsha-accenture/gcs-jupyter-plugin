15a6304e2944d922976a0b97987148ca111ac0c6
saranyaloganathan23 <saranya.lognathan@accenture.com>
2025-03-25 11:30:46 +0530

    Adding the skeleton code

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the gcs-jupyter-plugin extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'gcs-jupyter-plugin:plugin',
+  description: 'A JupyterLab extension.',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension gcs-jupyter-plugin is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('gcs-jupyter-plugin settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for gcs-jupyter-plugin.', reason);
+        });
+    }
+
+    requestAPI<any>('get-example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The gcs_jupyter_plugin server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
